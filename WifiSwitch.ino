3b0ff427a9e5cd0053aecb67705c6e3eb8191f91
Anatoly Duzenko <a.duzenko@wsmintl.com>
2024-12-18 09:24:46 -0500

    Code refactoring, added ability to store Wifi settings in EEPROM. Firmware does not work yet.

--- WifiSwitch.ino
+++ WifiSwitch.ino
@@ -9,19 +9,94 @@ extern "C" {
   #include "user_interface.h"
 }
 
-#define EEPROM_SIZE 96  // Size of EEPROM to store SSID and Password
-
-char ssid[32];       // Buffer for SSID (max length 32)
-char password[64];   // Buffer for Password (max length 64)
+// Default AP settings 
 const char* apSSID = "WifiSwitch";
 const char* apPassword = "12345678";
-const String self_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"; // Token
-const String serv_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"; // Server Token
-const String name = "WifiSwitch"; // Switch Name
-const String serverIP = "192.168.0.55"; // IP WEB 
-bool lamp_on =  false;
+const char* defaultUsername = "admin";
+const char* defaultPassword = "password";
+
+// Device settings
+#define EEPROM_SIZE 304
+
+struct DeviceConfig {
+  char wifiSSID[32]; // Wifi Network name
+  char wifiPassword[64]; // Wifi password
+  char deviceName[32]; // Device Name
+  char deviceUser[16]; // Device Username
+  char devicePassword[16]; // Device Password
+  char apiServerIP[16]; // API Server IP
+  char apiServerToken[64]; // API Server token
+  char authToken[64]; // Authorization token
+};
+
+DeviceConfig config; // Create an instance of the config struct
+
+bool load_on =  false;
 bool can_toggle = false;
 int button_state;
+
+
+// Forms (Login and Settings)
+const char* loginForm = R"rawliteral(
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>ESP8266 Login</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f4f4f9;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+      }
+      .login-container {
+        background: #fff;
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        width: 300px;
+      }
+      h2 {
+        text-align: center;
+      }
+      input {
+        width: 100%;
+        padding: 10px;
+        margin-bottom: 15px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+      }
+      button {
+        width: 100%;
+        padding: 10px;
+        background-color: #007BFF;
+        color: #fff;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+      }
+      button:hover {
+        background-color: #0056b3;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="login-container">
+      <h2>Login</h2>
+      <form method="POST" action="/login">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </body>
+  </html>
+  )rawliteral";
+
 const char settingsForm[] PROGMEM = R"rawliteral(
   <!DOCTYPE html>
   <html lang='en'>
@@ -56,13 +131,14 @@ const char settingsForm[] PROGMEM = R"rawliteral(
               margin-bottom: 5px;
           }
           input, button {
-              width: 100%;
+              width: 95%;
               padding: 10px;
               margin-bottom: 15px;
               border: 1px solid #ccc;
               border-radius: 5px;
           }
           button {
+              width: 100%;
               background-color: #007BFF;
               color: #fff;
               border: none;
@@ -78,12 +154,24 @@ const char settingsForm[] PROGMEM = R"rawliteral(
       <div class='container'>
           <h2>WifiSwitcher Settings</h2>
           <form action='/settings' method='POST'>
-              <label for='ssid'>Wifi SSID</label>
-              <input type='text' id='ssid' name='ssid' placeholder='Enter network name' required>
-
-              <label for='password'>Wifi PW</label>
-              <input type='password' id='password' name='password' placeholder='Enter password' required>
-
+              <label for='wifiSSID'>Wifi SSID</label>
+              <input type='text' id='wifiSSID' name='wifiSSID' placeholder='Enter wireless network name' required>
+              <label for='wifiPassword'>Wifi Password</label>
+              <input type='password' id='wifiPassword' name='wifiPassword' placeholder='Enter password' required>
+              <label for='deviceName'>Device Name</label>
+              <input type='text' id='deviceName' name='deviceName' placeholder='Enter switch name'>
+              <hr/>
+              <label for='apiServerIP'>API Server IP</label>
+              <input type='text' id='apiServerIP' name='apiServerIP' placeholder='Enter remote server IP'>
+              <label for='apiServerToken'>API Server Token</label>
+              <input type='text' id='apiServerToken' name='apiServerToken' placeholder='Enter remote server auth token'>
+              <hr/>
+              <label for='deviceUser'>Username</label>
+              <input type='password' id='deviceUser' name='deviceUser' placeholder='Enter username' required>
+              <label for='devicePassword'>Password</label>
+              <input type='password' id='devicePassword' name='devicePassword' placeholder='Enter password' required>
+              <label for='authToken'>Switch Auth Token</label>
+              <input type='text' id='authToken' name='authToken' placeholder='Enter switch auth token'>
               <button type='submit'>Save</button>
           </form>
       </div>
@@ -93,44 +181,56 @@ const char settingsForm[] PROGMEM = R"rawliteral(
 
 ESP8266WebServer server(80); // web-server
 HTTPClient http; // web-client
-WiFiClient wifi;
+WiFiClient wifi; // wifi
 
-const int lamp = 0; // Set output
-const int button = 2; // manual switch
+const int load = 0; // Set output pin
+const int button = 2; // Set input switch
 
-// Function to save Wi-Fi credentials to EEPROM
-void saveWiFiCredentials(const char* ssid, const char* password) {
-  EEPROM.begin(EEPROM_SIZE);  // Initialize EEPROM
-  for (int i = 0; i < 32; ++i) {
-    EEPROM.write(i, ssid[i]); // Write SSID
-  }
-  for (int i = 0; i < 64; ++i) {
-    EEPROM.write(32 + i, password[i]); // Write Password
-  }
-  EEPROM.commit();  // Save changes
-  Serial.println("Wi-Fi Credentials Saved to EEPROM");
-}
 
-// Function to load Wi-Fi credentials from EEPROM
-bool loadWiFiCredentials(char* ssid, char* password) {
+// Configuration functions 
+// Initialize the struct with sample data
+// void initializeConfig() {
+//   strcpy(config.wifiSSID, "MyWiFi");
+//   strcpy(config.wifiPassword, "12345678");
+//   strcpy(config.apiServerIP, "192.168.1.100");
+//   strcpy(config.apiServerToken, "abcdef1234567890");
+//   strcpy(config.deviceName, "WifiSwitch");
+//   strcpy(config.deviceUser, "admin");
+//   strcpy(config.devicePassword, "password");
+//   strcpy(config.authToken, "auth1234567890");
+// }
+
+// Save the struct to EEPROM
+void saveConfig() {
+  Serial.println("Saving configuration to EEPROM...");
   EEPROM.begin(EEPROM_SIZE);
-  bool valid = false;
-  for (int i = 0; i < 32; ++i) {
-    ssid[i] = EEPROM.read(i);
-    if (ssid[i] != 0xFF) valid = true; // Check for empty EEPROM
+  uint8_t* ptr = (uint8_t*)&config; // Pointer to the struct
+  for (int i = 0; i < sizeof(config); i++) {
+    EEPROM.write(i, *(ptr + i));
   }
-  for (int i = 0; i < 64; ++i) {
-    password[i] = EEPROM.read(32 + i);
-  }
-  ssid[31] = '\0';  // Ensure null-termination
-  password[63] = '\0';
-  EEPROM.end();
-  return valid; // Return if valid credentials exist
+  EEPROM.commit();
+  Serial.println("Configuration saved!");
 }
 
-// Internal web-server check
+// Load the struct from EEPROM
+bool loadConfig() {
+  EEPROM.begin(EEPROM_SIZE);
+  Serial.println("Loading configuration from EEPROM...");
+  uint8_t* ptr = (uint8_t*)&config; // Pointer to the struct
+  for (int i = 0; i < sizeof(config); i++) {
+    *(ptr + i) = EEPROM.read(i);
+  }
+
+  EEPROM.end();
+  // Validate by checking if `wifiName` is not empty
+  return (config.wifiSSID[0] != '\0');
+}
+
+
+// Web management interface
+// Authorization page
 void handleRoot() { 
-  server.send(200, "text/plain", "Hello! I am " + name);
+  server.send(200, "text/html", loginForm);
 }
 
 // Incorrect requests handler
@@ -139,73 +239,121 @@ void handleNotFound(){
   server.send(404, "text/plain", message);
 }
 
-// Switch On
-void turnOnLamp(){
-  digitalWrite(lamp, LOW);
-  lamp_on = true;
+// Authorization handler
+void handleLogin() {
+  if (server.method() == HTTP_POST) {
+    String username = server.arg("username");
+    String password = server.arg("password");
+
+    if (username == defaultUsername && password == defaultPassword) {
+      server.sendHeader("Location", String("/settings"), true);
+      server.send ( 302, "text/plain", "");
+    } else {
+      server.send(403, "text/html", "<h1>Login Failed!</h1><p>Invalid credentials. Please try again.</p><script type='text/javascript'>setTimeout(function(){ window.location = '/'; }, 200);</script>");
+    }
+  } else {
+    server.send(405, "text/plain", "Method Not Allowed");
+  }
 }
 
-// Switch Off
-void turnOffLamp(){
-  digitalWrite(lamp, HIGH);
-  lamp_on = false;
+// Settings form handler
+void handleSettingsForm(){
+  server.send(200, "text/html", settingsForm);
 }
 
-// Server update
+// Settings form saving handler
+void handleSettingsSave() {
+  if (server.method() == HTTP_POST) {
+    
+    String wifiSSID = server.arg("wifiName");
+    String wifiPassword = server.arg("wifiPassword");
+    String apiServerIP = server.arg("apiServerIP");
+    String apiServerToken = server.arg("apiServerToken");
+    String deviceName = server.arg("deviceName");
+    String deviceUser = server.arg("deviceUSer");
+    String devicePassword = server.arg("devicePassword");
+    String authToken = server.arg("authToken");
+
+    // Ensure no buffer overflows
+    wifiSSID.toCharArray(config.wifiSSID, sizeof(config.wifiSSID));
+    wifiPassword.toCharArray(config.wifiPassword, sizeof(config.wifiPassword));
+    apiServerIP.toCharArray(config.apiServerIP, sizeof(config.apiServerIP));
+    apiServerToken.toCharArray(config.apiServerToken, sizeof(config.apiServerToken));
+    deviceName.toCharArray(config.deviceName, sizeof(config.deviceName));
+    deviceUser.toCharArray(config.deviceUser, sizeof(config.deviceUser));
+    devicePassword.toCharArray(config.devicePassword, sizeof(config.devicePassword));
+    authToken.toCharArray(config.authToken, sizeof(config.authToken));
+
+    // Save the updated configuration to EEPROM
+    saveConfig();
+
+    // Send response back to the client
+    server.send(200, "application/json", "{\"status\":\"success\",\"message\":\"Configuration saved! Restart the device.\"}");
+  } else {
+    server.send(405, "application/json", "{\"status\":\"error\",\"message\":\"Method not allowed!\"}");
+  }
+}
+
+// API Server status update 
 void sendServer(bool state){
-  http.begin(wifi, "http://"+serverIP+"/iapi/setstate");
-  String post = "token="+self_token+"&state="+(state?"on":"off"); // По токену сервер будет определять что это за устройство
+  http.begin(wifi, "http://"+String(config.apiServerIP)+"/iapi/setstate");
+  String post = "token="+String(config.apiServerToken)+"&state="+(state?"on":"off"); // The API server will determine the device by token (ToDO)
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
   int httpCode = http.POST(post);
   http.end();  
 }
 
-// State toggle
-void toggleLamp(){
-  if(lamp_on == true) {
-    turnOffLamp();
-  //  sendServer(false);
-  } else {
-    turnOnLamp();
-  //  sendServer(true);
-  }
-}
-
-// Server handlers
+// Server load handlers
 void handleOn(){
   String token = server.arg("token");
-  if(serv_token != token) {
+  if(!token.equals(config.authToken)) {
     String message = "access denied";
     server.send(401, "text/plain", message);
     return;
   }
-  turnOnLamp();
+  turnLoadOn();
   String message = "success";
   server.send(200, "text/plain", message);
 }
 
 void handleOff(){
   String token = server.arg("token");
-  if(serv_token != token) {
+  if(!token.equals(config.authToken)) {
     String message = "access denied";
     server.send(401, "text/plain", message);
     return;
   }
-  turnOffLamp();
+  turnLoadOff();
   String message = "success";
   server.send(200, "text/plain", message);
 }
 
-void handleSettingsForm(){
-  server.send(200, "text/html", settingsForm);
+
+// Manipulations with the load
+// Switch On
+void turnLoadOn(){
+  digitalWrite(load, LOW);
+  load_on = true;
 }
 
-void handleSaveSettings() {
-  String ssid = server.arg("ssid");
-  String password = server.arg("password");
-  saveWiFiCredentials(ssid.c_str(), password.c_str());
-  server.send(200, "text/html", "Saved, restart the device!");
+// Switch Off
+void turnLoadOff(){
+  digitalWrite(load, HIGH);
+  load_on = false;
 }
+
+// State toggle
+void toggleLoadState(){
+  if(load_on == true) {
+    turnLoadOff();
+    sendServer(false);
+  } else {
+    turnLoadOn();
+    sendServer(true);
+  }
+}
+
+
 
 void initVariant() {
   uint8_t mac[6] = {0x00, 0xA3, 0xA0, 0x1C, 0x8C, 0x45};
@@ -213,15 +361,16 @@ void initVariant() {
 }
 
 void setup(void){
-  pinMode(lamp, OUTPUT);
-  pinMode(button, INPUT_PULLUP); // Важно сделать INPUT_PULLUP
-  turnOffLamp();
-  WiFi.mode(WIFI_STA);
-  if (loadWiFiCredentials(ssid, password) == true) {
-    WiFi.hostname(name);
-    WiFi.begin(ssid, password);
+  pinMode(load, OUTPUT);
+  pinMode(button, INPUT_PULLUP); // Important to make INPUT_PULLUP
+  turnLoadOff();
+  
+  if (loadConfig()) {
+    WiFi.mode(WIFI_STA);
+    WiFi.hostname(config.deviceName);
+    WiFi.begin(config.wifiSSID, config.wifiPassword);
 
-    // Ждем пока подключимся к WiFi
+    // Waiting for WiFi connection
     int attempts = 0;
     while (WiFi.status() != WL_CONNECTED && attempts < 20) {
       delay(500);
@@ -229,18 +378,19 @@ void setup(void){
     }
   } 
   
-  WiFi.mode(WIFI_AP); // Set to Access Point mode
-  WiFi.softAP(apSSID, apPassword);
+  if(WiFi.status() != WL_CONNECTED)  {
+    WiFi.mode(WIFI_AP); // Set to Access Point mode
+    WiFi.softAP(apSSID, apPassword);
+  }
   
-  // Назначем функции на запросы
+  // Configure routes
   server.on("/", handleRoot);
   server.on("/on",  HTTP_POST, handleOn);
   server.on("/off", HTTP_POST, handleOff);
+  server.on("/login", HTTP_POST, handleLogin);
   server.on("/settings", HTTP_GET, handleSettingsForm);
-  server.on("/settings", HTTP_POST, handleSaveSettings);
+  server.on("/settings", HTTP_POST, handleSettingsSave);
   server.onNotFound(handleNotFound);
-
-  // Стартуем сервер
   server.begin();
 
   // OTA Setup
@@ -269,10 +419,9 @@ void loop(void){
   ArduinoOTA.handle();
   server.handleClient();
 
-  // Проверяем нажатие выключателя
   button_state = digitalRead(button);
   if (button_state == HIGH && can_toggle) {
-    toggleLamp();
+    toggleLoadState();
     can_toggle = false;
     delay(500);
   } else if(button_state == LOW){
